a5b75f53c875b3d3fc3c7ef3907b3caed6722ef7
ThatDot <justinsanveloso@gmail.com>
2026-04-28 06:04:15 +0800

    chore: update astro and dev config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    port: 5173,
+  },
   base: "/justin-portfolio/",
   vite: {
     plugins: [tailwindcss()],
